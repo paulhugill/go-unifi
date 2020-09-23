5df4d3278f7fd0f7a6f1f064c7176192d56d8a50
Paul Hugill <paul@hugill.me>
2020-09-23 08:16:45 +0100

    split out do for doLogin

--- unifi/unifi.go
+++ unifi/unifi.go
@@ -53,7 +53,7 @@ func (c *Client) Login(ctx context.Context, user, pass string) error {
 		c.c.Jar = jar
 	}
 
-	err := c.do(ctx, "POST", "api/auth/login", &struct {
+	err := c.doLogin(ctx, "POST", "api/auth/login", &struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
 	}{
@@ -62,6 +62,69 @@ func (c *Client) Login(ctx context.Context, user, pass string) error {
 	}, nil)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (c *Client) doLogin(ctx context.Context, method, relativeURL string, reqBody interface{}, respBody interface{}) error {
+	var (
+		reqReader io.Reader
+		err       error
+		reqBytes  []byte
+	)
+	if reqBody != nil {
+
+		reqBytes, err = json.Marshal(reqBody)
+		if err != nil {
+			return fmt.Errorf("unable to marshal JSON: %s %s %w", method, relativeURL, err)
+		}
+		reqReader = bytes.NewReader(reqBytes)
+	}
+
+	reqURL, err := url.Parse(relativeURL)
+	if err != nil {
+		return fmt.Errorf("unable to parse URL: %s %s %w", method, relativeURL, err)
+	}
+
+	url := c.baseURL.ResolveReference(reqURL)
+
+	req, err := http.NewRequestWithContext(ctx, method, url.String(), reqReader)
+	if err != nil {
+		return fmt.Errorf("unable to create request: %s %s %w", method, relativeURL, err)
+	}
+
+	req.Header.Set("User-Agent", "terraform-provider-unifi/0.1")
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+
+	resp, err := c.c.Do(req)
+	if err != nil {
+		return fmt.Errorf("unable to perform request: %s %s %w", method, relativeURL, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotFound {
+		return &NotFoundError{}
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Printf("Request Body:\n%s\n", string(reqBytes))
+		errBody := struct {
+			Meta meta `json:"meta"`
+		}{}
+		err = json.NewDecoder(resp.Body).Decode(&errBody)
+		return fmt.Errorf("%s %s (%s) for %s %s", errBody.Meta.RC, errBody.Meta.Message, resp.Status, method, url.String())
+	}
+
+	if respBody == nil || resp.ContentLength == 0 {
+		return nil
+	}
+
+	// TODO: check rc in addition to status code?
+
+	err = json.NewDecoder(resp.Body).Decode(respBody)
+	if err != nil {
+		return fmt.Errorf("unable to decode body: %s %s %w", method, relativeURL, err)
 	}
 
 	return nil
@@ -95,7 +158,6 @@ func (c *Client) do(ctx context.Context, method, relativeURL string, reqBody int
 	}
 
 	req.Header.Set("User-Agent", "terraform-provider-unifi/0.1")
-	req.Header.Add("Content-Type", "application/json; charset=utf-8")
 
 	resp, err := c.c.Do(req)
 	if err != nil {
